0cc8bcb10203c3a4d1b304a0e0fd2ca257b0105a
jiwanchoi <54405518+jiwanchoi@users.noreply.github.com>
2023-09-05 16:30:37 +0900

    vending_js

--- JS/vending/index.js
+++ JS/vending/index.js
@@ -1,5 +1,8 @@
-import ColaGenerator from "./colaGenerator";
+import ColaGenerator from "./js/colaGenerator.js";
+import VendingMachineFunc from "./js/vendingMachineFunc.js";
 
 const colaGenerator = new ColaGenerator();
+await colaGenerator.setup();
 
-colaGenerator.setup();
+const vendingMachine = new VendingMachineFunc();
+vendingMachine.setup();
