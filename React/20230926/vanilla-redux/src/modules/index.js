ca041975fbc263d44bcae613a0858a330e98373d
jiwanchoi <wldhks303@naver.com>
2023-09-27 17:13:31 +0900

    tdd

--- React/20230926/vanilla-redux/src/modules/index.js
+++ React/20230926/vanilla-redux/src/modules/index.js
@@ -1,11 +1,7 @@
 import goodsReducer from "./goodsCounter";
 import stockReducer from "./stockCounter";
 import { combineReducers } from 'redux';
-import App from "./App"
-import {createStore} from 'redux';
-import roodReducer from "./modules";
 
-const store = createStore(rootReducer);
 
 const rootReducer = combineReducers({
     goodsReducer,
