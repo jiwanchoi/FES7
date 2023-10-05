493334d0686b06b2ab33c2b7f540e41600aa3bf2
jiwanchoi <wldhks303@naver.com>
2023-10-05 11:01:57 +0900

    webpack

---
+++ React/20231004/account-manager/src/components/ExpenseTotal/ExpenseTotal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ExpenseTotal() {
+  return (
+    <div>
+    
+    <span>총 지출 : </span>
+    <span>원</span>
+    <span>잔액 : </span>
+    <span>원</span>
+    </div>
+  )
+}
